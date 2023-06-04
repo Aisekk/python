b485bc04b56a8966d8df5574b420a4ee61f555eb
Alexander Chekmarev <gooletters@gmail.com>
2023-06-04 17:23:08 +0300

    Added aliens fleet

--- E_Matthes/alien_invasion/alien_invasion.py
+++ E_Matthes/alien_invasion/alien_invasion.py
@@ -1,6 +1,8 @@
 import pygame
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -8,19 +10,30 @@ def run_game():
 	# Инициализирует pygame, settings и объект экрана.
 	pygame.init()
 	ai_settings = Settings()
-	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	screen = pygame.display.set_mode((ai_settings.screen_width, 
+	ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+	# Создание экземпляра для хранения игровой статистики.
+	stats = GameStats(ai_settings)
 	# Назначение цвета фона.
 	bg_color = (230, 230, 230)
-	# Создание корабля.
+	
+	# Создание корабля, группы пуль и группы пришельцев.
 	ship = Ship(ai_settings, screen)
-	# Создание группы для хранения пуль.
 	bullets = Group()
+	aliens = Group()
+	
+	# Создание флота пришельцев.
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+
 	# Запуск основного цикла игры.
 	while True:
 		gf.check_events(ai_settings, screen, ship, bullets)
-		ship.update()
-		gf.update_bullets(bullets)
-		gf.update_screen(ai_settings, screen, ship, bullets)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, stats, screen, ship, aliens, 
+			bullets)
+			gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- E_Matthes/alien_invasion/settings.py
+++ E_Matthes/alien_invasion/settings.py
@@ -9,10 +9,16 @@ class Settings():
 		self.bg_color = (230, 230, 230)
 		# Настройки корабля
 		self.ship_speed_factor = 1.5
+		self.ship_limit = 3
 		# Параметры пули
 		self.bullet_speed_factor = 1
-		self.bullet_width = 3
+		self.bullet_width = 300
 		self.bullet_height = 15
 		self.bullet_color = 60, 60, 60
 		self.bullets_allowed = 3
+		# Настройки пришельцев
+		self.alien_speed_factor = 0.05
+		self.fleet_drop_speed = 2
+		# fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+		self.fleet_direction = 1
 
